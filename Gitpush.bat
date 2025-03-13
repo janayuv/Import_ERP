f1098f8d1c3a5837e1a3e284e864bb2ae148a3e6
janayuv <bjana.ads@gmail.com>
2025-03-14 00:23:19 +0530

    Update backend and frontend code; add new database files

---
+++ Gitpush.bat
@@ -0,0 +1,10 @@
+git add .
+git commit -m "Update backend and frontend code; add new database files"
+git push
+git push origin master
+
+cd erp-frontend
+git add .
+git commit -m "Update backend and frontend code; add new database files"
+git push
+git push origin master
